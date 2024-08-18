1139ac8f77c1de7553967d26712f9d16aa17543a
RobAb2023 <abreuroberta093.freelancer@gmail.com>
2024-08-18 20:41:46 -0300

    inicio projeto

---
+++ roh1000midias/src/main/java/com/netlify/app/blog/roh1000midias/Roh1000midiasApplication.java
@@ -0,0 +1,13 @@
+package com.netlify.app.blog.roh1000midias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roh1000midiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roh1000midiasApplication.class, args);
+	}
+
+}
---
+++ roh1000midias/src/test/java/com/netlify/app/blog/roh1000midias/Roh1000midiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netlify.app.blog.roh1000midias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roh1000midiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
